d81840f8f42d44c55ba5481876664618edb27509
Juan Manuel Herrera Carrera <tampiko@gmail.com>
2018-11-30 15:42:24 -0600

    Se eliminaron archivos innecesarios, se agrego font awesome, ya genera los archivos seleccionados y permite seleccionar por lotes

--- assets/app/js/index.js
+++ assets/app/js/index.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 });
 
 $("#bdatos").change(function(){
+	resetForm();
 	getTablasDeBD();
 });
 
@@ -17,24 +18,27 @@ $("#btnGeneraModel").click(function(){
 $("#btnGeneraController").click(function(){
 	generaController();
 });
+
 $("#btnGeneraJS").click(function(){
-	btnGeneraJS();
-});
-
-$("#btnDescargar").click(function(){
-	descargar();
-});
-
-$("#btnGeneraDoble").click(function(){
-	/* Crear pero con promesas */
-	generaController();
-	generaModel();
+	generaJS();
 });
 
 $('#modalError').on('shown.bs.modal', function(e){
 	setTimeout(function(){
 		$("#modalError").modal('hide');
 	}, 1500);
+});
+
+$("#btnSelTodosControlador").click(function(){
+	SeleccionaTodos('controladores');
+});
+
+$("#btnSelTodosModelo").click(function(){
+	SeleccionaTodos('modelos');
+});
+
+$("#btnSelTodosJs").click(function(){
+	SeleccionaTodos('js');
 });
 
 $(function(){
@@ -50,7 +54,13 @@ function descargar(){
 			nombre: $("#bdatos").val()
 		}
 	}).done(function(res){
-		$("#divMensajes").append(`<a target="_blank" href="${res.url}${urlServer}gens/${res.zip}">Descargar</a><br>`);
+		$("#divBtnZip").addClass('oculto');
+		$("#divMensajes").html(`
+			<a target="_blank" class="btn btn-primary" href="${res.url}${urlServer}gens/${res.zip}">
+				<span class="fa fa-download"></span> Descargar
+			</a>
+		`);
+		
 	}).fail(function(){
 	}).always(function(){
 	});
@@ -83,10 +93,32 @@ function getTablasDeBD(){
 		}
 	}).done(function(res){
 		$("#tabla").html('');
+		$("#tcontenidoTablas").html('');
 		$.each(res.data, function(index, el){
-			$("#tabla").append(`<option value="${el.table_name}">${el.table_name}</option>`);
+			$("#tabla").append(`<option value="${el.table_name.toLowerCase()}">${el.table_name.toLowerCase()}</option>`);
+			$("#tcontenidoTablas").append(`
+			<tr>
+				<td width="70%" class="text-monospace">${el.table_name.toLowerCase()}</td>
+				<td width="10%" class="text-monospace text-center" id="cC${el.table_name.toLowerCase()}">
+					<input type="checkbox" class="${el.table_name.toLowerCase()} controladores" id="controladores" value="${el.table_name.toLowerCase()}" checked="checked">
+				</td>
+				<td width="10%" class="text-monospace text-center" id="cM${el.table_name.toLowerCase()}">
+					<input type="checkbox" class="${el.table_name.toLowerCase()} modelos" id="modelos" value="${el.table_name.toLowerCase()}" checked="checked">
+				</td>
+				<td width="10%" class="text-monospace text-center" id="cJ${el.table_name.toLowerCase()}">
+					<input type="checkbox" class="${el.table_name.toLowerCase()} js" id="js" value="${el.table_name.toLowerCase()}" checked="checked">
+				</td>
+				<td>
+					<button type="button" class="btn btn-info fa fa-hand-o-left" onclick="SeleccionaTodos('${el.table_name.toLowerCase()}');"></button>
+				</td>
+			</tr>`);
 		});
 		$("#tabla").val(0);
+		$("#divMensajes").html('<button class="btn btn-info" id="btnGenerarArchivos">Generar Archivos</button>');
+		$("#btnGenerarArchivos").click(function(){
+			generarArchivos();
+		});
+		$("#tTablas").removeClass('oculto');
 	}).fail(function(){
 	}).always(function(){
 	});
@@ -109,9 +141,7 @@ function getInfoTabla(){
 	});
 }
 
-function generaController(){
-	tabla = $("#tabla").val();
-	console.log(tabla);
+function generaController(tabla){
 	if(tabla == null){
 		$("#modalError").modal('show');
 	}else{
@@ -127,16 +157,17 @@ function generaController(){
 				info: JSON.stringify(info)
 			}
 		}).done(function(res){
-			$("#divMensajes").append(`Archivo creado: application\\controllers\\${tabla}.php<br>`);
+			$("#divMensajes").html(`Archivo creado: application\\controllers\\${tabla}.php<br>`);
+			$("#cC" + tabla.toLowerCase()).html(`<span class="fa fa-check"></span>`);
+			$("#cuantos").val(parseInt($("#cuantos").val()) + 1);
+			revisa();
 		}).fail(function(){
 		}).always(function(){
 		});
 	}
 }
 
-function generaModel(){
-	tabla = $("#tabla").val();
-	console.log(tabla);
+function generaModel(tabla){
 	if(tabla == null){
 		$("#modalError").modal('show');
 	}else{
@@ -152,16 +183,17 @@ function generaModel(){
 				info: JSON.stringify(info)
 			}
 		}).done(function(res){
-			$("#divMensajes").append(`Archivo creado: application\\models\\${tabla}_model.php<br>`);
+			$("#divMensajes").html(`Archivo creado: application\\models\\${tabla}_model.php<br>`);
+			$("#cM" + tabla.toLowerCase()).html(`<span class="fa fa-check"></span>`);
+			$("#cuantos").val(parseInt($("#cuantos").val()) + 1);
+			revisa();
 		}).fail(function(){
 		}).always(function(){
 		});
 	}
 }
 
-function btnGeneraJS(){
-	tabla = $("#tabla").val();
-	console.log(tabla);
+function generaJS(tabla){
 	if(tabla == null){
 		$("#modalError").modal('show');
 	}else{
@@ -177,9 +209,60 @@ function btnGeneraJS(){
 				info: JSON.stringify(info)
 			}
 		}).done(function(res){
-			$("#divMensajes").append(`Archivo creado: app\\js\\${tabla}.js<br>`);
+			$("#divMensajes").html(`Archivo creado: app\\js\\${tabla}.js<br>`);
+			$("#cJ" + tabla.toLowerCase()).html(`<span class="fa fa-check"></span>`);
+			$("#cuantos").val(parseInt($("#cuantos").val()) + 1);
+			revisa();
 		}).fail(function(){
 		}).always(function(){
 		});
+	}
+}
+
+function generarArchivos(){
+	$('input[type=checkbox]:checked').each(function(){
+		$("#todos").val(parseInt($("#todos").val()) + 1);
+		switch($(this).prop("id")){
+			case 'controladores':
+				generaController($(this).val());
+				break;
+			case 'modelos':
+				generaModel($(this).val());
+				break;
+			case 'js':
+				generaJS($(this).val());
+				break;
+		}
+	});
+	if(parseInt($("#todos").val()) == 0){
+		$("#modalError").modal('show');
+	}
+}
+
+function resetForm(){
+	$("#divBtnZip").addClass('oculto');
+	$("#tcontenidoTablas").html('');
+	$("#divMensajes").html('');
+}
+
+function revisa(){
+	if(parseInt($("#todos").val()) == parseInt($("#cuantos").val())){
+		$("#divBtnZip").removeClass('oculto');
+		$("#divMensajes").html(`<button class="btn btn-outline-primary" type="button" id="btnDescargar">Genera Link de Descarga</button>`);
+		$("#btnDescargar").click(function(){
+			descargar();
+		});
+	}
+}
+
+function SeleccionaTodos(tipo){
+	checkboxes = $("." + tipo);
+	estado = 0;
+	for(i = 0, n = checkboxes.length; i < n; i++){
+		if(estado == 0){
+			estado = 1;
+			estadoNow = !checkboxes[i].checked;
+		}
+		checkboxes[i].checked = estadoNow;
 	}
 }
